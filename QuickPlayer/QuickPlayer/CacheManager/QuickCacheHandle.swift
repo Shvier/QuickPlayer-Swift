8bb046d3f5abe5b5338da9e2e85a5373662ee3be
Shvier <daulvi@hotmail.com>
2017-04-14 17:52:51 +0800

    Implement cache handle

--- QuickPlayer/QuickPlayer/CacheManager/QuickCacheHandle.swift
+++ QuickPlayer/QuickPlayer/CacheManager/QuickCacheHandle.swift
@@ -8,6 +8,70 @@
 
 import UIKit
 
-class QuickCacheHandle: NSObject {
+// default cache path: /Library/Caches/com.Shvier.QuickPlayer/xxx.mp4
+// default temp file path: /Library/Caches/com.Shvier.QuickPlayer/xxx
 
+public class QuickCacheHandle: NSObject {
+    
+    static let fileManager = FileManager.default
+    static let cachePath = NSHomeDirectory().appending("/Library/Caches/\(Bundle.main.bundleIdentifier!)")
+
+    static open func createTempFile(filename: String) -> Bool {
+        let filePath = QuickCacheHandle.tempFilePath(filename: filename)
+        if fileManager.fileExists(atPath: filePath) {
+            do {
+                try fileManager.removeItem(atPath: filePath)
+            } catch let error {
+                print("remove temp file error: \(error)")
+            }
+        }
+        return fileManager.createFile(atPath: filePath, contents: nil, attributes: nil)
+    }
+    
+    static open func writeTempFile(data: Data, filename: String) {
+        let fileHandle = FileHandle(forWritingAtPath: QuickCacheHandle.tempFilePath(filename: filename))
+        fileHandle?.seekToEndOfFile()
+        fileHandle?.write(data)
+    }
+    
+    static open func readTempFileData(offset: UInt64, length: UInt64, filename: String) -> Data {
+        let fileHandle = FileHandle(forReadingAtPath: QuickCacheHandle.tempFilePath(filename: filename))
+        fileHandle?.seek(toFileOffset: offset)
+        return (fileHandle?.readData(ofLength: Int(length)))!
+    }
+    
+    static open func cacheTempFile(filename: String) {
+        do {
+            let cacheFilePath = "\(cacheFolderPath)/\(filename)"
+            try fileManager.copyItem(atPath: QuickCacheHandle.tempFilePath(filename: filename), toPath: cacheFilePath)
+            print("cache file success")
+        } catch let error {
+            print("cache file error: \(error)")
+        }
+    }
+    
+    static open func clearCache() {
+        do {
+            try fileManager.removeItem(atPath: QuickCacheHandle.cacheFolderPath())
+        } catch let error {
+            print("clear cache error: \(error)")
+        }
+    }
+    
+    static private func tempFilePath(filename: String) -> String {
+        return "\(QuickCacheHandle.cacheFolderPath())/\(filename).tmp"
+    }
+    
+    static open func cacheFolderPath() -> String {
+        let isDirectory = UnsafeMutablePointer<ObjCBool>.allocate(capacity: 1)
+        if !fileManager.fileExists(atPath: cachePath, isDirectory: isDirectory) {
+            do {
+                try fileManager.createDirectory(atPath: cachePath, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("create cache folder error: \(error)")
+            }
+        }
+        return cachePath
+    }
+    
 }
---
+++ QuickPlayer/QuickPlayer/ResourceManager/QuickSessionTaskProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  QuickSessionTaskProtocol.swift
+//  QuickPlayer
+//
+//  Created by Shvier on 14/04/2017.
+//  Copyright © 2017 Shvier. All rights reserved.
+//
+
+import Foundation
+
+@objc public protocol QuickSessionTaskDelegate {
+    
+}
