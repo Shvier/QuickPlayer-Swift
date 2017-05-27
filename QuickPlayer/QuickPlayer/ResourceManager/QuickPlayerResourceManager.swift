6e656d8b889766eaac46cc3add8c3fe27e6a6d3a
Shvier <daulvi@hotmail.com>
2017-05-27 21:50:43 +0800

    Add URLSessionDataDelegate Save

--- QuickPlayer/QuickPlayer/ResourceManager/QuickPlayerResourceManager.swift
+++ QuickPlayer/QuickPlayer/ResourceManager/QuickPlayerResourceManager.swift
@@ -10,5 +10,46 @@ import UIKit
 import AVFoundation
 
 class QuickPlayerResourceManager: NSObject {
+    
+    lazy var session: URLSession = {
+        return URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.main)
+    }()
+    
+    private(set) public var videoURL: URL!
+    public weak var delegate: QuickPlayerResourceManagerDelegate!
 
+    public init(_ videoURL: URL, delegate: QuickPlayerResourceManagerDelegate) {
+        super.init()
+        self.videoURL = videoURL
+        self.delegate = delegate
+    }
+    
+    func resume() {
+        
+    }
+    
+    func suspend() {
+        
+    }
+    
+    func cancel() {
+        
+    }
+
+}
+
+extension QuickPlayerResourceManager: URLSessionDataDelegate {
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        
+    }
+    
 }
